e20ea3488c5fb3e4116b250e3e1eb58080575f96
LordGurr <50996232+LordGurr@users.noreply.github.com>
2021-04-25 17:09:25 +0200

    Add project files.

---
+++ Chiffer2/Game1.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Chiffer2
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private Rectangle rectangle;
+        private Texture2D square;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            rectangle = new Rectangle(Window.ClientBounds.Width / 2 - 50, Window.ClientBounds.Height / 2 - 20, 100, 40);
+            square = Content.Load<Texture2D>("Square4");
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Green);
+            _spriteBatch.Begin(SpriteSortMode.Deferred, null, SamplerState.PointClamp);
+            _spriteBatch.Draw(square, rectangle, Color.Orange);
+            // TODO: Add your drawing code here
+            _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
